4a4c5a3bbdce73e54c6225c6558e21adf37db6d6
JeongwooHam <hamjw0122@gmail.com>
2024-02-13 18:48:44 +0900

    'deploytogh-pages'

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = withNextra()
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+module.exports = withNextra({
+  assetPrefix: isProd ? "https://mobi-docs.github.io/mobi-docs/" : "",
+  reactStrictMode: true,
+  images: {
+    loader: "imgix",
+    path: "",
+  },
+});
