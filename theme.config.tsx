95a37293633d903bab9b27590afaa29421979e81
JeongwooHam <hamjw0122@gmail.com>
2024-02-13 17:44:09 +0900

    ✨ feat: Nextra 정적 사이트 생성 및 기본 페이지 구현

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>MOBI DOCS</span>,
+  project: {
+    link: "https://github.com/mobi-community/mobi-docs",
+  },
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  docsRepositoryBase: "https://github.com/mobi-community/mobi-docs",
+  footer: {
+    text: "MOBI DOCS",
+  },
+};
+
+export default config;
